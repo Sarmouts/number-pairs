0ac3573456779c8cd9f00a26f81643ce7e1b45ba
Sarmouts <saradis.m06@gmail.com>
2021-09-23 19:57:42 +0300

    Add project files.

---
+++ number pairs/number pairs.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	while (true)
+	{
+		int a;
+		cout << "How many numbers do you have: ";
+		cin >> a;
+		if (a <= 0)
+		{
+			cout << "Your number must be positive" << endl;
+			cout << "==========================================================================================================" << endl;
+		}
+		else if (a % 2 != 0)
+		{
+			cout << abs(a) / 2 << " pairs of numbers and " << abs(a) % 2 << " non pair number, his position: " << abs(a) / 2 + 1 << endl;
+			cout << "==========================================================================================================" << endl;
+		}
+		else
+		{
+			cout << abs(a) / 2 << " pairs of numbers and " << abs(a) % 2 << " non pair numbers" << endl;
+			cout << "==========================================================================================================" << endl;
+		}
+	}
+	return 0;
+}
+
